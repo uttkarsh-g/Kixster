8e5bc5a636244a991fd9cb013bfcbb9e9689a5b5
Alexhawak <hloji999@gmail.com>
2025-02-07 23:04:11 +0530

    make about responsive

--- assets/animation.js
+++ assets/animation.js
@@ -1,111 +1,105 @@
 import 'remixicon/fonts/remixicon.css';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// gsap.registerPlugin(ScrollTrigger);
 
-// // hero
+// // // // hero
 
-gsap.from('.title h1', {
-  delay: 0.5,
-  y: 50,
-  opacity: 0,
-});
+// gsap.from('.title h1', {
+//   delay: 0.5,
+//   y: 50,
+//   opacity: 0,
+// });
 
-gsap.from('.img img', {
-  delay: 1,
-  y: -150,
-  opacity: 0,
-});
-gsap.to('.imge', {
-  y: '50px',
-  opacity: 0,
-  scrollTrigger: {
-    trigger: '.imge',
-    scroller: 'body',
-    start: '50% 50%',
-    end: '50% 30%',
-    scrub: 5,
-  },
-});
-gsap.to('.title', {
-  y: '-20%',
-  scrollTrigger: {
-    trigger: '.title',
-    scroller: 'body',
-    start: '50% 25%',
-    end: '50% 10%',
-    scrub: 5,
-  },
-});
+// gsap.from('.img img', {
+//   delay: 1,
+//   y: -150,
+//   opacity: 0,
+// });
+// gsap.to('.imge', {
+//   y: '50px',
 
-gsap.to('.btn', {
-  x: '10%',
-  scrollTrigger: {
-    trigger: '.btn',
-    scroller: 'body',
-    start: '50% 80%',
-    end: '50% 60%',
-    scrub: 5,
-  },
-});
+//   scrollTrigger: {
+//     trigger: '.imge',
+//     scroller: 'body',
+//     start: '50% 50%',
+//     end: '50% 30%',
+//     scrub: 5,
+//   },
+// });
+// gsap.to('.title', {
+//   y: '-20%',
+//   scrollTrigger: {
+//     trigger: '.title',
+//     scroller: 'body',
+//     start: '50% 25%',
+//     end: '50% 10%',
+//     scrub: 5,
+//   },
+// });
+// gsap.from('.btn', {
+//   opacity: 0,
+//   delay: 2,
+// });
 
-gsap.from('.scrol', {
-  opacity: 0,
-  delay: 2,
-});
-gsap.to('.bar', {
-  y: 25,
-  duration: 1,
-  repeat: -1,
-  yoyo: true,
-});
+// gsap.from('.scrol', {
+//   opacity: 0,
+//   delay: 2,
+// });
+// gsap.to('.bar', {
+//   delay: 2.5,
+//   y: 25,
+//   duration: 1,
+//   repeat: -1,
+//   yoyo: true,
+// });
 
-// // about section
-let mm = gsap.matchMedia();
+// // // // about section
+// let horizontalScroll = gsap.matchMedia();
 
-mm.add('(min-width: 600px)', () => {
-  const hori = gsap.utils.toArray('#about .h');
+// horizontalScroll.add('(min-width: 769px)', () => {
+//   const hori = gsap.utils.toArray('#about .h');
 
-  gsap.to(hori, {
-    xPercent: -100 * (hori.length - 1),
-    delay: 2,
-    scrollTrigger: {
-      trigger: '#about',
-      pin: true,
-      scrub: 1,
-      end: '+=3500',
-    },
-  });
+//   gsap.to(hori, {
+//     xPercent: -100 * (hori.length - 1),
+//     delay: 2,
+//     scrollTrigger: {
+//       trigger: '#about',
+//       pin: true,
+//       scrub: 1,
+//       end: '+=3500',
+//     },
+//   });
 
-  gsap.from('.la', {
-    y: 50,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: '.la',
-      scroller: 'body',
-      start: '0% 30%',
-      end: '10% 20%',
-      scrub: 3,
-    },
-  });
-  gsap.from('.aa', {
-    x: 500,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: '.aa',
-      scroller: 'body',
-      start: '80% 30%',
-      scrub: 3,
-    },
-  });
-  gsap.from('.fa', {
-    opacity: 0,
-    scrollTrigger: {
-      trigger: '.fa',
-      scroller: 'body',
-      start: '2055px 30%',
-      end: '+=3500',
-      scrub: 3,
-    },
-  });
-});
+//   gsap.from('.la', {
+//     y: 50,
+//     opacity: 0,
+//     scrollTrigger: {
+//       trigger: '.la',
+//       scroller: 'body',
+//       start: '0% 30%',
+//       end: '10% 20%',
+//       scrub: 3,
+//     },
+//   });
+//   gsap.from('.aa', {
+//     y: -500,
+//     opacity: 0,
+//     scrollTrigger: {
+//       trigger: '.aa',
+//       scroller: 'body',
+//       start: '80% 30%',
+//       scrub: 3,
+//     },
+//   });
+//   gsap.from('.fa', {
+//     opacity: 0,
+//     scrollTrigger: {
+//       trigger: '.fa',
+//       scroller: 'body',
+//       start: '2055px 30%',
+//       end: '+=3500',
+//       scrub: 3,
+//     },
+//   });
+// });
--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,40 @@
 const m = document.querySelector('.menu');
 const h = document.querySelector('.hamburger');
+const navlinks = document.querySelectorAll('.hamburger a');
+const cards = document.querySelectorAll('.card');
 const b = document.body;
 const btn = document.querySelectorAll('.box');
 const img = document.querySelectorAll('.img');
 const mouse = document.querySelector('.mm');
+const heart = document.querySelectorAll('.heart');
 
 // Mouse Follower
 
-// function move(x, y) {
-//   mouse.style.left = x - 10 + 'px';
-//   mouse.style.top = y - 8 + 'px';
-// }
-// const hide = () => {
-//   mouse.style.opacity = '0';
-// };
-// const show = () => {
-//   mouse.style.opacity = 1;
-// };
-// b.addEventListener('mouseenter', show);
-// b.addEventListener('mouseleave', hide);
-// b.addEventListener('mousemove', (arg) => {
-//   move(arg.x, arg.y);
-// });
+function move(x, y) {
+  mouse.style.left = x - 10 + 'px';
+  mouse.style.top = y - 8 + 'px';
+}
+const hide = () => {
+  mouse.style.opacity = '0';
+};
+const show = () => {
+  mouse.style.opacity = 1;
+};
+b.addEventListener('mouseenter', show);
+b.addEventListener('mouseleave', hide);
+b.addEventListener('mousemove', (arg) => {
+  move(arg.x, arg.y);
+});
 
-// // mobile touch
-// b.addEventListener('touchstart', show);
-// b.addEventListener('touchmove', (e) => {
-//   const touch = e.touches[0];
-//   move(touch.clientX, touch.clientY);
-// });
-// b.addEventListener('touchend', hide);
+// mobile touch
+b.addEventListener('touchstart', show);
+b.addEventListener('touchmove', (e) => {
+  const touch = e.touches[0];
+  move(touch.clientX, touch.clientY);
+});
+b.addEventListener('touchend', hide);
 
-//Hamburger
+// Hamburger
 
 m.addEventListener('click', (e) => {
   e.stopPropagation();
@@ -40,6 +43,14 @@ m.addEventListener('click', (e) => {
   b.classList.toggle('stop');
 });
 
+navlinks.forEach((navLinks) => {
+  navLinks.addEventListener('click', (e) => {
+    e.stopPropagation();
+    m.classList.toggle('mact');
+    h.classList.toggle('active');
+    b.classList.toggle('stop');
+  });
+});
 // Hero images
 
 img.forEach((e, i) => {
@@ -62,5 +73,25 @@ btn.forEach((ele, index) => {
         image.style.left = '100%';
       }
     });
+  });
+});
+
+// cards
+
+cards.forEach((element) => {
+  element.addEventListener('mouseenter', (e) => {
+    e.stopPropagation();
+    mouse.style.opacity = 0;
+  });
+  element.addEventListener('mouseleave', (e) => {
+    e.stopPropagation();
+    mouse.style.opacity = 1;
+  });
+});
+
+heart.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    e.stopPropagation();
+    icon.classList.toggle('love');
   });
 });
