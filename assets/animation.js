035fe9205e71bcbd3aa28f1c1693d2408693337c
Alexhawak <hloji999@gmail.com>
2025-02-25 12:58:48 +0530

    finish

--- assets/animation.js
+++ assets/animation.js
@@ -13,98 +13,98 @@ gsap.to('nav', {
     scrub: 1,
   },
 });
-// gsap.from('.title h1', {
-//   delay: 0.5,
-//   y: 50,
-//   opacity: 0,
-// });
+gsap.from('.title h1', {
+  delay: 0.5,
+  y: 50,
+  opacity: 0,
+});
 
-// gsap.from('.img img', {
-//   delay: 1,
-//   y: -250,
-//   opacity: 0,
-// });
-// gsap.to('.imge', {
-//   y: '50px',
+gsap.from('.img img', {
+  delay: 1,
+  y: -250,
+  opacity: 0,
+});
+gsap.to('.imge', {
+  y: '50px',
 
-//   scrollTrigger: {
-//     trigger: '.imge',
-//     scroller: 'body',
-//     start: '50% 50%',
-//     end: '50% 30%',
-//     scrub: 5,
-//   },
-// });
-// gsap.to('.title', {
-//   y: '-20%',
-//   scrollTrigger: {
-//     trigger: '.title',
-//     scroller: 'body',
-//     start: '50% 25%',
-//     end: '50% 10%',
-//     scrub: 5,
-//   },
-// });
-// gsap.from('.btn', {
-//   opacity: 0,
-//   delay: 2,
-// });
+  scrollTrigger: {
+    trigger: '.imge',
+    scroller: 'body',
+    start: '50% 50%',
+    end: '50% 30%',
+    scrub: 5,
+  },
+});
+gsap.to('.title', {
+  y: '-20%',
+  scrollTrigger: {
+    trigger: '.title',
+    scroller: 'body',
+    start: '50% 25%',
+    end: '50% 10%',
+    scrub: 5,
+  },
+});
+gsap.from('.btn', {
+  opacity: 0,
+  delay: 2,
+});
 
-// gsap.from('.scrol', {
-//   opacity: 0,
-//   delay: 2,
-// });
-// gsap.to('.bar', {
-//   delay: 2.5,
-//   y: 25,
-//   duration: 1,
-//   repeat: -1,
-//   yoyo: true,
-// });
+gsap.from('.scrol', {
+  opacity: 0,
+  delay: 2,
+});
+gsap.to('.bar', {
+  delay: 2.5,
+  y: 25,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+});
 
-// // // // about section
-// let horizontalScroll = gsap.matchMedia();
+// // // about section
+let horizontalScroll = gsap.matchMedia();
 
-// horizontalScroll.add('(min-width: 769px)', () => {
-//   const hori = gsap.utils.toArray('#about .h');
+horizontalScroll.add('(min-width: 769px)', () => {
+  const hori = gsap.utils.toArray('#about .h');
 
-//   gsap.to(hori, {
-//     xPercent: -100 * (hori.length - 1),
-//     delay: 5,
-//     scrollTrigger: {
-//       trigger: '#about',
-//       pin: true,
-//       scrub: 1,
-//       end: '+=4000',
-//     },
-//   });
+  gsap.to(hori, {
+    xPercent: -100 * (hori.length - 1),
+    delay: 5,
+    scrollTrigger: {
+      trigger: '#about',
+      pin: true,
+      scrub: 1,
+      end: '+=4000',
+    },
+  });
 
-//   gsap.from('.la', {
-//     y: 50,
-//     opacity: 0,
-//     scrollTrigger: {
-//       trigger: '.la',
-//       scroller: 'body',
-//       start: '0% 30%',
-//       end: '10% 20%',
-//     },
-//   });
-//   gsap.from('.aa', {
-//     y: -500,
-//     opacity: 0,
-//     scrollTrigger: {
-//       trigger: '.aa',
-//       scroller: 'body',
-//       start: '80% 30%',
-//     },
-//   });
-//   gsap.from('.fa', {
-//     opacity: 0,
-//     scrollTrigger: {
-//       trigger: '.fa',
-//       scroller: 'body',
-//       start: '2055px 30%',
-//       end: '+=3500',
-//     },
-//   });
-// });
+  gsap.from('.la', {
+    y: 50,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.la',
+      scroller: 'body',
+      start: '0% 30%',
+      end: '10% 20%',
+    },
+  });
+  gsap.from('.aa', {
+    y: -500,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.aa',
+      scroller: 'body',
+      start: '80% 30%',
+    },
+  });
+  gsap.from('.fa', {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.fa',
+      scroller: 'body',
+      start: '2055px 30%',
+      end: '+=3500',
+    },
+  });
+});
